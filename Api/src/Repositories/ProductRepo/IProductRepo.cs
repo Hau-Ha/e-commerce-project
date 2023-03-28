bcef3cb8eea614b3177298582d132313823c2554
HauHa <haduyhau1995@gmail.comm>
2023-03-28 06:28:20 +0300

    Update UserRepo and ProductRepo and BaseRepo

---
+++ Api/src/Repositories/ProductRepo/IProductRepo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.src.Models;
+using Api.src.Repositories.BaseRepo;
+
+namespace Api.src.Repositories.ProductRepo
+{
+    public interface IProductRepo : IBaseRepo<Product>
+    {
+        
+    }
+}
---
+++ Api/src/Repositories/UserRepo/UserRepo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.src.Database;
+using Api.src.Models;
+using Api.src.Repositories.BaseRepo;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.src.Repositories.UserRepo
+{
+    public class UserRepo : BaseRepo<User>, IUserRepo
+    {
+         private readonly DatabaseContext _context;
+
+        public UserRepo(DatabaseContext context) : base(context)
+        {
+            _context = context;
+        }
+
+       public async Task<int> GetTotal()
+{
+    return await _context.Users.CountAsync();
+}
+    }
+}
