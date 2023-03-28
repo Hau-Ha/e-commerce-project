5d5a66a362327d98fa2fcc2a020a1affbf41b062
HauHa <haduyhau1995@gmail.comm>
2023-03-28 07:07:34 +0300

    fix

---
+++ Api/src/Services/BaseService/BaseService.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.src.Services.BaseService;
+
+public class BaseService
+{
+    
+}
---
+++ Api/src/Services/BaseService/IBaseService.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.src.Repositories.BaseRepo;
+
+namespace Api.src.Services.BaseService;
+{
+    public interface IBaseService<T, TReadDto, TCreateDto, TUpdateDto>
+{
+    Task<IEnumerable<TReadDto>> GetAllAsync(QueryOptions options);
+        Task<TReadDto?> GetByIdAsync(string id);
+        Task<TReadDto> UpdateOneAsync(string id, TUpdateDto update);
+        Task<bool> DeleteOneAsync(string id);
+        Task<TReadDto> CreateOneAsync (TCreateDto create);
+}
+ 
+
+}
+
