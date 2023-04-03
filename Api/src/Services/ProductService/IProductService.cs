8d9536c9a43165586bc61c39edf79963d6eb329e
HauHa <haduyhau1995@gmail.comm>
2023-04-04 02:29:55 +0300

    FixlayOut

--- Api/src/Services/ProductService/IProductService.cs
+++ Api/src/Services/ProductService/IProductService.cs
@@ -8,7 +8,6 @@ namespace Api.src.Services.ProductService
 {
     public interface IProductService : IBaseService<Product, ProductReadDto, ProductCreateDto, ProductUpdateDto>
     {
-       Task<IEnumerable<ProductReadDto>> GetProductsByCategoryIdAsync(Guid categoryId , QueryOptions options ) ;
-     
+        Task<IEnumerable<ProductReadDto>> GetProductsByCategoryIdAsync(Guid categoryId, QueryOptions options);
     }
 }
